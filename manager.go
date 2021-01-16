c372889adb8101296e027c271d1c502259081941
Elis Lulja <elulja@cisco.com>
2021-01-16 14:50:19 +0100

    Implemente Manager
    Signed-off-by: Elis Lulja <elulja@cisco.com>

---
+++ manager.go
@@ -0,0 +1,113 @@
+// Copyright © 2021 Elis Lulja
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hitcount
+
+import "sync"
+
+var (
+	managersList map[string]CounterManager = map[string]CounterManager{}
+)
+
+// CounterManager is in charge of handling different counters.
+type CounterManager interface {
+	Add(string, Counter) CounterManager
+	Remove(string) CounterManager
+	Reset(...string) CounterManager
+	Get(string) Counter
+}
+
+type mgr struct {
+	counters map[string]Counter
+	lock     sync.Mutex
+}
+
+// Manager returns a CounterManager or creates a new one if it doesn't exist.
+func Manager(key string) CounterManager {
+	m, exists := managersList[key]
+	if !exists {
+		managersList[key] = &mgr{
+			counters: map[string]Counter{},
+		}
+		m = managersList[key]
+	}
+
+	return m
+}
+
+func (m *mgr) Add(key string, count Counter) CounterManager {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if len(key) == 0 || count == nil {
+		return m
+	}
+
+	if _, exists := m.counters[key]; exists {
+		return m
+	}
+
+	m.counters[key] = count
+	return m
+}
+
+func (m *mgr) Remove(key string) CounterManager {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if len(key) == 0 {
+		return m
+	}
+
+	if _, exists := m.counters[key]; !exists {
+		return m
+	}
+
+	delete(m.counters, key)
+	return m
+}
+
+func (m *mgr) Reset(except ...string) CounterManager {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	for key, counter := range m.counters {
+		ignore := false
+
+		for _, exp := range except {
+			if key == exp {
+				ignore = true
+			}
+		}
+		if ignore {
+			continue
+		}
+
+		counter.Reset()
+	}
+
+	return m
+}
+
+func (m *mgr) Get(key string) Counter {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	counter, exists := m.counters[key]
+	if !exists {
+		return nil
+	}
+
+	return counter
+}
---
+++ manager_test.go
@@ -0,0 +1,124 @@
+// Copyright © 2021 Elis Lulja
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hitcount
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestManager(t *testing.T) {
+	a := assert.New(t)
+	key := "key"
+	newKey := "new-key"
+
+	res := Manager(key)
+	a.Equal(&mgr{counters: map[string]Counter{}}, res)
+	a.Len(managersList, 1)
+	m, exists := managersList[key]
+	a.True(exists)
+	a.Equal(m, res)
+
+	managersList[newKey] = &mgr{
+		counters: map[string]Counter{
+			"test": NewIntCounter(55, 100),
+		},
+	}
+	res = Manager(newKey)
+	a.Equal(managersList[newKey], res)
+	a.Len(managersList, 2)
+}
+
+func TestAdd(t *testing.T) {
+	a := assert.New(t)
+	key := "key"
+	m := &mgr{
+		counters: map[string]Counter{
+			"test": NewIntCounter(55, 100),
+		},
+	}
+
+	m.Add(key, nil)
+	a.Len(m.counters, 1)
+
+	counter := NewIntCounter(77, 100)
+	m.Add(key, counter)
+	a.Len(m.counters, 2)
+	counterRes := m.Get(key)
+	a.Equal(counter, counterRes)
+
+	newCounter := NewIntCounter(88, 100)
+	m.Add(key, newCounter)
+	a.Len(m.counters, 2)
+	counterRes = m.Get(key)
+	a.Equal(counter, counterRes)
+}
+
+func TestRemove(t *testing.T) {
+	a := assert.New(t)
+	key := "exists"
+	notExists := "not-exists"
+	start := map[string]Counter{
+		key: NewIntCounter(55, 100),
+	}
+	m := &mgr{
+		counters: start,
+	}
+
+	m.Remove("")
+	a.Equal(m.counters, start)
+
+	m.Remove(notExists)
+	a.Equal(m.counters, start)
+
+	m.Remove(key)
+	a.Empty(m.counters)
+}
+
+func TestManagerReset(t *testing.T) {
+	a := assert.New(t)
+	exceptions := []string{"one", "two"}
+	start := map[string]Counter{
+		exceptions[0]: NewIntCounter(50, 100),
+		exceptions[1]: NewIntCounter(60, 100),
+		"stays":       NewIntCounter(70, 100),
+	}
+
+	m := &mgr{
+		counters: start,
+	}
+
+	m.Reset(exceptions...)
+	a.Equal(m.counters, map[string]Counter{
+		exceptions[0]: NewIntCounter(50, 100),
+		exceptions[1]: NewIntCounter(60, 100),
+		"stays":       NewIntCounter(0, 100),
+	})
+}
+
+func TestGet(t *testing.T) {
+	a := assert.New(t)
+	start := map[string]Counter{
+		"one": NewIntCounter(70, 100),
+	}
+
+	m := &mgr{
+		counters: start,
+	}
+
+	a.NotNil(m.Get("one"))
+	a.Nil(m.Get("two"))
+}
